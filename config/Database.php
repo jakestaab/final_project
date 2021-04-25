247996fefb218bc27be5daa68c2e422b29039b3f
Jake Staab <jjstaab@mail.fhsu.edu>
2021-04-25 15:24:10 -0500

    minor error fixed

--- config/Database.php
+++ config/Database.php
@@ -1,11 +1,23 @@
 <?php
 
     class Database {
+        
+        /*
         private $host = 'localhost';
-        private $db_name = 'quoteapi';
+        private $db_name = 'quotesdb';
         private $username = 'root';
         private $password = 'sesame';
         private $conn;
+        */
+        
+        
+        private $host = ' 	g84t6zfpijzwx08q.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+        private $db_name = 'pofqsjx4clotr29u';
+        private $username = 'rdycj9qyn2kew2op';
+        private $password = 'syo1y3gsrn3jlcm9';
+        private $conn;
+        
+
 
         public function connect() {
             $this->conn = null;

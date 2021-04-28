4f4e8037b83f08c43dc713b8b40c954ee15b2726
Jake Staab <jjstaab@mail.fhsu.edu>
2021-04-28 08:06:21 -0500

    test

--- index.php
+++ index.php
@@ -13,9 +13,63 @@ if ($action == NULL) {
     }
 }
 
+$database = new Database();
+$db = $database->connect();
+
+$post = new Quotes($db);
 
 if($action == 'list_quotes') {
+    $authorId = filter_input(INPUT_GET, 'authorId', FILTER_VALIDATE_INT);
+    $categoryId = filter_input(INPUT_GET, 'categoryId', FILTER_VALIDATE_INT);
 
+    //keep these
+    $authors = Author::get_authors();
+    $categories = Category::get_categories();
+
+    if(isset($_GET['authorId']) && $authorId != 0 && $categoryId == 0) {
+        $post->authorId = $_GET['authorId'];
+        $result = $post->read_single_author($authorId);
+    } else if ($authorId == 0 && isset($_GET['categoryId']) && $categoryId != 0) {
+        $post->categoryId = $_GET['categoryId'];
+        $result = $post->read_single_category($categoryId);
+    } else if ($categoryId == 0 && $authorId == 0) {
+        $result = $post->read();
+    } else if (isset($_GET['authorId']) && isset($_GET['categoryId'])) {
+        $post->authorId = $_GET['authorId'];
+        $post->categoryId = $_GET['categoryId'];
+        $result = $post->read_category_and_author($categoryId, $authorId);
+    }
+
+    $num = $result->rowCount();
+
+    
+    if($num > 0) {
+        $posts_arr = array();
+        $posts_arr['data'] = array();
+
+        while($row = $result->fetch(PDO::FETCH_ASSOC)) {
+            extract($row);
+
+            $post_item = array(
+                'id' => $id,
+                'quote' => $quote,
+                'category_name' => $category_name,
+                'author_name' => $author_name,
+            );
+
+            // push to "data"
+            array_push($posts_arr['data'], $post_item);
+        }
+    } else {
+        $posts_arr['data'] = array();
+        $post_item = array(
+            'id' => '',
+            'quote' => 'No Results',
+            'category_name' => '',
+            'author_name' => '',
+        );
+        array_push($posts_arr['data'], $post_item);
+    }
     
     include('view/quotes_list.php');
 } else if ($action == 'order_by') {
--- view/quotes_list.php
+++ view/quotes_list.php
@@ -6,7 +6,50 @@
 </head>
 <body>
     <main>
-        <p>TEST</p>
+        <div id="intro">
+        <h1 style="font-size: 250%;">Jake Staab's Quotes</h1>
+        <p style="font-size: 75%">(written by other, more famous people)</p>
+        </div><br>
+        <div class="">
+                <form>
+                    <input type="hidden" name="action" value="order_by">
+                    <select name="authorId">
+                        <option value="0">View All Authors</option>
+                        <?php foreach ($authors as $author) { ?>
+                            <option value="<?php echo $author['id']; ?>">
+                                <?php echo $author['author']; ?>
+                            </option>
+                        <?php } ?>
+                    </select>
+                    <select name="categoryId">
+                        <option value="0">View All Categories</option>
+                        <?php foreach ($categories as $category) { ?>
+                            <option value="<?php echo $category['id']; ?>">
+                                <?php echo $category['category']; ?>
+                            </option>
+                        <?php } ?>
+                    </select>
+
+                    <input class="" type="submit" value="Submit" />
+                </form>
+            </div>
+
+        <table>
+            <?php foreach($posts_arr['data'] as $r) {
+                $quote = $r['quote'];
+                $author = $r['author_name'];
+                $category = $r['category_name'];
+            ?>
+            <tr>
+                <td class="quote"><?php echo $quote ?></td>
+                <td class="category"><?php echo $category ?></td>
+            </tr>
+            <tr>
+                <td class="author"><?php echo $author ?></td>
+                
+            </tr>
+            <?php } ?>
+        </table>
     </main>
 </body>
 </html>

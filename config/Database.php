534ea8bc04aa0c973f86be4e44eeeca633f0d661
Jake Staab <jjstaab@mail.fhsu.edu>
2021-04-28 12:07:06 -0500

    changes to database file

--- config/Database.php
+++ config/Database.php
@@ -2,25 +2,20 @@
 
     class Database {
         
-        
+        /*
         private $host = 'localhost';
         private $db_name = 'quotesdb';
         private $username = 'root';
         private $password = 'sesame';
         private $conn;
+        */
         
         
-        /*
         private $host = 'g84t6zfpijzwx08q.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
         private $db_name = 'pofqsjx4clotr29u';
         private $username = 'rdycj9qyn2kew2op';
         private $password = 'syo1y3gsrn3jlcm9';
         private $conn;
-        private static $dsn = 'mysql:host=g84t6zfpijzwx08q.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=pofqsjx4clotr29u';
-        private static $username1 = 'rdycj9qyn2kew2op';
-        private static $password1 = 'syo1y3gsrn3jlcm9';
-        private static $db;
-        */
 
 
         public function connect() {

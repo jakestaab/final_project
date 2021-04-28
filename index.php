6be7eff729c6e14099ffaf2006ad111d60d4283d
Jake Staab <jjstaab@mail.fhsu.edu>
2021-04-28 07:36:41 -0500

    test

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-header('Access-Control-Allow-Origin: *');
+//header('Access-Control-Allow-Origin: *');
 require('./config/database.php');
 require('./models/Author.php');
 require('./models/Category.php');
